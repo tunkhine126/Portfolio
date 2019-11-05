3294445556d760244ca20ab96788ee255f686bc6
tunkhine126 <tunkhine126@gmail.com>
2019-11-05 15:52:33 -0500

    SCSS changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,19 +8,16 @@ const Projects = () => {
     <div id="projects" className="projects">
       <container className="container" fluid>
         <h2> PROJECTS </h2>
-
             <Row>
               {ProjectsData.map(project =>
                 <Col>
                   <Card className="projectCard" border="none" project={project} key={project.id} style={{ width: '20rem', height: '25rem'}}>
                     <div className="face face1">
                       <div className="content">
-                    <Card.Img variant="top" src={project.img_url} className="projectImage"/>
-                    
-                    </div>
+                        <Card.Img variant="top" src={project.img_url} className="projectImage"/>
                       </div>
-
-                      <div className="face face2">
+                    </div>
+                    <div className="face face2">
                       <h4>
                         <Card.Title as='h4'>
                           <Card.Link href={project.project_url}>{project.name}</Card.Link>
@@ -34,16 +31,14 @@ const Projects = () => {
                           {project.github_frontend ? <Card.Link href={project.github_frontend}>GitHub frontend </Card.Link> : ""}
                           {project.github_backend ? <Card.Link href={project.github_backend}>GitHub backend </Card.Link> : ""}
                       </Card.Body>
-                      </div>
-                      
-                    </Card>
+                    </div>
+                  </Card>
                 </Col>
               )}
             </Row>
         <br />
-
       </container>  
     </div>
   )
-  }
+}
   export default Projects;
