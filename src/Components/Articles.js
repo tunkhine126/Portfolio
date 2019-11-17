51cd29e091ca29a18bed3cf7867cf31a198a2822
tunkhine126 <tunkhine126@gmail.com>
2019-11-17 13:36:17 -0500

    Articles overhaul

---
+++ src/Components/Articles.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ArticleCard from './ArticleCard'
+import ArticlesData from '../Data/ArticlesData'
+
+
+
+const Articles = () => {
+  return (
+    <div id="articles" className="articles">
+      <h2>RECENT ARTICLES</h2>
+
+      <div className="articles-container">
+        {console.log(ArticlesData)}
+        {ArticlesData.map(article => <ArticleCard article={article} key={article.id} />)}
+        {/* {ArticlesData.map(article => <ArticleCard article={article} key={article.id} />)} */}
+      </div>
+    </div>
+  )
+}
+
+export default Articles;
