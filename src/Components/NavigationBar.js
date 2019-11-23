028131d6b5dbdcaa69318cf84931091646c28d9c
tunkhine126 <tunkhine126@gmail.com>
2019-11-23 13:06:35 -0500

    Nav adjustments

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -8,7 +8,9 @@ const NavigationBar = () => {
 return (
   <div>
     <Navbar className="navBar" bg="dark" variant="dark" fixed="top">
-      <Navbar.Brand className="navbar-name" href="#top"><FaCode className="code-icon" size={35} style={{ color: '#A031FF' }} /></Navbar.Brand>
+      <Navbar.Brand className="navbar-name" href="#top">
+        <FaCode className="code-icon" size={35} style={{ color: '#A031FF' }} />
+      </Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
         <Nav.Link className="navbar-links" href="#aboutMe">About me</Nav.Link>
         <Nav.Link className="navbar-links" href="#projects">Projects</Nav.Link>
